4a97cccf34381dc74e5ea936b698650c1e09b6c1
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-01 16:18:19 +0530

    fix test setup file

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,14 +1,6 @@
-import { expect, afterEach } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import matchers from "@testing-library/jest-dom/matchers";
-
-if (!matchers) {
-    console.error(
-        "Error: Unable to load matchers from @testing-library/jest-dom."
-    );
-} else {
-    expect.extend(matchers);
-}
+import "@testing-library/jest-dom/vitest";
 
 afterEach(() => {
     cleanup();
