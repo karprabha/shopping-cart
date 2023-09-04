af42d068cdb269334ff7946637ae1f659a483fa5
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-04 17:57:16 +0530

    add dropdown select for category

--- src/components/container/CardContainer.tsx
+++ src/components/container/CardContainer.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { CategorizedItems } from "../../types/CategorizedItems";
 import Card from "../card/Card";
 import SkeletonCard from "../skeleton/SkeletonCard";
@@ -9,16 +11,42 @@ interface CardContainerProps {
 const CardContainer: React.FC<CardContainerProps> = ({ categorizedItems }) => {
     const loadingSkeletons = Array.from({ length: 14 });
 
+    const [selectedCategory, setSelectedCategory] = useState("all");
+
+    const handleCategoryChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    console.log(categorizedItems);
+
     return (
-        <div className="card-container">
-            {categorizedItems["all"].length > 0
-                ? categorizedItems["all"].map((item) => (
-                      <Card key={item.id} item={item} />
-                  ))
-                : loadingSkeletons.map((_, index) => (
-                      <SkeletonCard key={index} />
-                  ))}
-        </div>
+        <>
+            <Link to={"/"}>Home</Link>
+            <span> &gt; Products &gt; </span>
+            <select
+                name="select-category"
+                id="select-category-dropdown"
+                value={selectedCategory}
+                onChange={handleCategoryChange}
+            >
+                <option value="all">All</option>
+                <option value="jewelery">Jewelery</option>
+                <option value="women's clothing">Women's Clothing</option>
+                <option value="men's clothing">Men's Clothing</option>
+            </select>
+
+            <div className="card-container">
+                {categorizedItems[selectedCategory].length > 0
+                    ? categorizedItems[selectedCategory].map((item) => (
+                          <Card key={item.id} item={item} />
+                      ))
+                    : loadingSkeletons.map((_, index) => (
+                          <SkeletonCard key={index} />
+                      ))}
+            </div>
+        </>
     );
 };
 
