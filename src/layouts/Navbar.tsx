28e615ed4ba1aa78bc6f9f91558bec1546129385
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-06 01:03:27 +0530

    add mobile hamburger menu

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import ShopContext from "../context/ShopContext";
 
@@ -6,9 +6,15 @@ const Navbar = () => {
     const { cartItems } = useContext(ShopContext);
     const itemCount = Object.keys(cartItems).length;
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav>
-            <ul>
+            <ul className={"desktop-menu"}>
                 <li>
                     <NavLink to={`/`}>Home</NavLink>
                 </li>
@@ -24,6 +30,35 @@ const Navbar = () => {
                     </NavLink>
                 </li>
             </ul>
+
+            <div
+                className={`hamburger-menu ${isMenuOpen ? "active" : ""}`}
+                onClick={toggleMenu}
+            >
+                ☰
+                {itemCount > 0 && !isMenuOpen && (
+                    <span className="item-count">{itemCount}</span>
+                )}
+            </div>
+
+            <div className={`dropdown-menu ${isMenuOpen ? "active" : ""}`}>
+                <ul className="menu-items" onClick={toggleMenu}>
+                    <li>
+                        <NavLink to={`/`}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/products`}>Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/cart`}>
+                            🛒 Cart{" "}
+                            {itemCount > 0 && (
+                                <span className="item-count">{itemCount}</span>
+                            )}
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
