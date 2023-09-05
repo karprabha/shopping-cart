50d864540728a5402b38a5d18dc38bca834b8342
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-09-05 18:57:57 +0530

    add API call logic in home section

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,62 @@
+import { useEffect, useMemo } from "react";
+import { useFetchAll, useLocalStorage } from "../hooks/customHooks";
+import { Item } from "../types/Item";
+import { CategorizedItems } from "../types/CategorizedItems";
+import Slider from "../components/slider/Slider";
 import HeroSection from "../layouts/home/HeroSection";
+import categorizeItemsByCategory from "../utils/categorizeItemsByCategory";
 
 const Home = () => {
+    const baseUrl = "https://fakestoreapi.com/products/category/";
+
+    const urls = useMemo(() => {
+        const categoryNames = [
+            "men's clothing",
+            "women's clothing",
+            "jewelery",
+        ];
+        return categoryNames.map(
+            (category) => `${baseUrl}${encodeURIComponent(category)}`
+        );
+    }, [baseUrl]);
+
+    const { results, loadingBatch, errorBatch } = useFetchAll<Item[]>(urls);
+
+    const items = results.flatMap((result) => result.data) as Item[];
+    const categorizedItems = categorizeItemsByCategory(items);
+
+    const [cachedCategorizedItems, setCachedCategorizedItems] =
+        useLocalStorage<CategorizedItems>("products", categorizedItems);
+
+    useEffect(() => {
+        if (
+            categorizedItems.all.length > 0 &&
+            JSON.stringify(cachedCategorizedItems) !==
+                JSON.stringify(categorizedItems)
+        ) {
+            setCachedCategorizedItems(categorizedItems);
+        }
+    }, [categorizedItems, cachedCategorizedItems, setCachedCategorizedItems]);
+
+    if (!loadingBatch && errorBatch) console.log(errorBatch);
+
     return (
         <div className="home-route">
             <HeroSection />
+            <div className="related-items">
+                <h3>Recommended for You</h3>
+                <Slider slides={cachedCategorizedItems["all"]} />
+            </div>
+
+            <div className="related-items">
+                <h3>Men's Category</h3>
+                <Slider slides={cachedCategorizedItems["men's clothing"]} />
+            </div>
+
+            <div className="related-items">
+                <h3>Women's Category</h3>
+                <Slider slides={cachedCategorizedItems["women's clothing"]} />
+            </div>
         </div>
     );
 };
